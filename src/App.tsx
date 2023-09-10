7c73c6b409a04bc45c23287252442646ea486e96
Jethro Coma <jethrocoma@gmail.com>
2023-09-09 22:50:16 -0700

    format code

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import './App.css'
-import { data } from '../public/fma-data'; 
+import './App.css';
+import { data } from '../public/fma-data';
 import { Header } from './components/header/Header';
 import { CharacterRatings } from './components/character_ratings/ChracterRatings';
 import CharacterCards from './components/character_cards/CharacterCards';
 
 function App() {
-
   return (
     <>
       <Header />
-      <CharacterRatings characters={data}/>
+      <CharacterRatings characters={data} />
       <CharacterCards characters={data} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
